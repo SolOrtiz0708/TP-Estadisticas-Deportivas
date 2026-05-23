ec54b81534c240efbaf82b8cb782af74ecd2d01c
liss <solsonnleintner@gmail.com>
2026-05-23 13:17:48 -0300

    Se agrega script de analisis
    Se crea el archivo analisis.py para procesar los datos deportivos.

--- analisis.py
+++ analisis.py
@@ -1,7 +1,23 @@
-ventas = [100, 200, 150, 300]
+import csv
 
-total = sum(ventas)
-max_venta = max(ventas)
+total_goles = 0
+goles_por_jugador = {}
 
-print("Total vendido:", total)
-print("Mayor venta:", max_venta)
+with open('../datos/estadisticas.csv', newline='') as archivo:
+    lector = csv.DictReader(archivo)
+    
+    for fila in lector:
+        jugador = fila['jugador']
+        goles = int(fila['goles'])
+        
+        total_goles += goles
+        
+        if jugador in goles_por_jugador:
+            goles_por_jugador[jugador] += goles
+        else:
+            goles_por_jugador[jugador] = goles
+
+max_jugador = max(goles_por_jugador, key=goles_por_jugador.get)
+
+print("Total de goles:", total_goles)
+print("Jugador con más goles:", max_jugador)
